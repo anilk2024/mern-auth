8ab4c5ce950b47545af94f58dbd2a79bdb9af587
Anil Yadav <Anil.Yadav@smartenergywater.in>
2024-02-17 12:40:14 +0530

    Conected with mongo

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from "express-async-handler";
+//import User from "../models/userModel";
 
 // @desc    Auth user & get token
 // @route   POST /api/users/auth
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { notFound,errorHandler } from './middleware/errorMiddlleleware.js';
+import connectDB from './config/db.js';
 dotenv.config();
 import userRoutes from './routes/userRoutes.js'
-
 const port = process.env.PORT||5000;
+connectDB();
 const app=express();
 
 app.use('/api/users',userRoutes);
