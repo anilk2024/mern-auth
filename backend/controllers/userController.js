264059f217b733ab6c64dfdeb24ba29a783d25c7
anilk2022 <pfms.anilkumar@gmail.com>
2024-02-17 22:06:49 +0530

    backend part completed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,28 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js"
+import genrateToken from "../utills/genrateToken.js";
+
 //import User from "../models/userModel";
 
 // @desc    Auth user & get token
 // @route   POST /api/users/auth
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Auth User" });
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    genrateToken(res, user._id);
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email
+    })
+  }
+  else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
+
 });
 
 // @desc    Register a new user
@@ -13,7 +30,25 @@ const authUser = asyncHandler(async (req, res) => {
 // @access  Public
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Register User" });
+  const { name, email, password } = req.body;
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    res.status(400);
+    throw new Error('User already exist.');
+  }
+  const user = await User.create({ name, email, password });
+  if (user) {
+    genrateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email
+    })
+  }
+  else {
+    res.status(400);
+    throw new Error('Invalid user data.');
+  }
 });
 
 // @desc    Logout user / clear cookie
@@ -21,7 +56,11 @@ const registerUser = asyncHandler(async (req, res) => {
 // @access  Public
 
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "logout User" });
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0)
+  });
+  res.status(200).json({ message: "User logged out. " });
 });
 
 // @desc    Get user profile
@@ -29,7 +68,12 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @access  Private
 
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get User Profile" });
+  const user = {
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email
+  }
+  res.status(200).json(user);
 });
 
 // @desc    Update user profile
@@ -37,7 +81,26 @@ const getUserProfile = asyncHandler(async (req, res) => {
 // @access  Private
 
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Update User profile" });
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
 });
 
 export {
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,27 @@
-import express from 'express';
+import express, { json } from 'express';
 import dotenv from 'dotenv';
-import { notFound,errorHandler } from './middleware/errorMiddlleleware.js';
+import { notFound, errorHandler } from './middleware/errorMiddlleleware.js';
 import connectDB from './config/db.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import userRoutes from './routes/userRoutes.js'
-const port = process.env.PORT||5000;
+const port = process.env.PORT || 8000;
+
 connectDB();
-const app=express();
 
-app.use('/api/users',userRoutes);
+const app = express();
 
-app.get('/',(req,res)=>{
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+app.use('/api/users', userRoutes);
+
+app.get('/', (req, res) => {
     res.send("Server is ready..");
 });
 
 app.use(notFound);
 app.use(errorHandler);
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server is running at port ${port}`)
 });
