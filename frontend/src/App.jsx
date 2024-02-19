1278573a21393864f7f8eb0a49901e9c8225e494
anilk2022 <pfms.anilkumar@gmail.com>
2024-02-19 08:15:33 +0530

    Created front end

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import { Container } from 'react-bootstrap'
+import Header from './components/Header';
+import { Outlet } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Container className='my-2'>
+        <Outlet />
+      </Container>
+
+    </>
+  );
+}
+
+export default App;
